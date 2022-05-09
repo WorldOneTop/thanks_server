c7db4f91228d764e86e0798f1ba35ba674fb3dad
WorldOneTop <dlwpdlf147@naver.com>
2022-05-09 12:00:22 +0000

    changed fcm account

--- thanks/firebase.py
+++ thanks/firebase.py
@@ -46,23 +46,3 @@ def sendAndCatchErr(message, token):
         
     except messaging.UnregisteredError as e: # 유효하지 않은 토큰 https://firebase.google.com/docs/reference/admin/python/firebase_admin.messaging?hl=ko
         Message.objects.get(pk=token).delete()
-
-# def send_to_firebase_cloud_messaging():
-#     # This registration token comes from the client FCM SDKs.
-#     registration_token = 'deWxx1ytQ2i4wUnZ-l6im2:APA91bG0nhfKfRuLc5nxZ9mSGQtEovHHM-2YkKTuKVu-E3bOGB0Hihf_tjZSo6zsGhhjrPoDa_ae2yg247q-_7idfGcbepSTF5qzqbrnSivVtnALJv_ZNOzvgiD1RtlpREKXpKjWl0U3'
-
-#     # See documentation on defining a message payload.
-#     message = messaging.Message(
-#     notification=messaging.Notification(
-#         title='안녕하세요 타이틀 입니다',
-#         body='안녕하세요 메세지 입니다',
-#     ),
-#     token=registration_token,
-#     )
-
-#     try:
-#         response = messaging.send(message)
-#         # Response is a message ID string.
-#         print('Successfully sent message:', response)
-#     except Exception as e:
-#         print('예외가 발생했습니다.', e)
